7cd36591d8d840138b593ce882f532c3c308de2d
browning <bwbrowning@gmail.com>
2013-10-29 20:31:52 -0500

    first commit

---
+++ static/app/app.js
@@ -0,0 +1,90 @@
+var xoryApp = angular.module('xoryApp', [
+  'ngRoute',
+]);
+
+xoryApp.factory('questionService', function($http) { 
+  var result; 
+  return {
+      qdata : function(callback) {
+          if (!result) {
+             console.log('lodeit');
+              result = $http.get('static/question.json').success(callback);  
+          }
+          return result;
+      },
+      refreshQdata : function (callback) {
+         return $http.get('static/question.json').success(callback);  
+      },
+      setData: function(data) {
+        result = data;
+      }
+  };
+});
+ 
+xoryApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/question/:name', {
+        templateUrl: 'static/question.html',
+        controller: 'QuestionCtrl'
+      }).
+      when('/answer', {
+        templateUrl: 'static/answer.html',
+        controller: 'QuestionCtrl'
+      }).
+      when('/result', {
+        templateUrl: 'static/result.html',
+        controller: 'QuestionCtrl'
+      }).
+      otherwise({
+        redirectTo: '/questions'
+      });
+  }]);
+
+function QuestionCtrl($scope, $location, questionService) {
+  $scope.qdata = questionService.qdata(function(results) {
+        $scope.qdata = results;
+  }); 
+  
+
+  $scope.answer1 = function() {
+    $location.path("answer")
+    if ($scope.qdata.questions[$scope.qdata.x].answer == "x") {
+      $scope.qdata.rightAnswers = $scope.qdata.rightAnswers + 1
+      $scope.qdata.res = true
+      $scope.qdata.answer = $scope.qdata.xchoice
+    }
+    else {
+      $scope.qdata.res = false
+      $scope.qdata.answer = $scope.qdata.ychoice
+    }
+    $scope.qdata.x = $scope.qdata.x + 1
+    questionService.setData($scope.qdata)
+    return false;
+  }
+
+  $scope.answer2 = function () {
+    $location.path("answer")
+    if ($scope.qdata.questions[$scope.qdata.x].answer == "y") {
+      $scope.qdata.rightAnswers = $scope.qdata.rightAnswers + 1
+      $scope.qdata.res = true
+    }
+    else {
+      $scope.qdata.res = false
+    }
+    $scope.qdata.x = $scope.qdata.x + 1
+    questionService.setData($scope.qdata)
+    return false;
+  }
+
+  $scope.nextQuestion = function() {
+    if ($scope.qdata.x == $scope.qdata.questions.length ) {
+      $location.path("result")
+    }
+    else {
+      $location.path("question/:name")
+    }
+  }
+};
+
+
