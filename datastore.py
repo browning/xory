6f7189a72e06e4512a1bc18bf3bfe55ac596857c
browning <bwbrowning@gmail.com>
2013-11-25 21:04:59 -0600

    Force mongo client to point at env variable

--- datastore.py
+++ datastore.py
@@ -21,7 +21,6 @@ def load_quiz(mongo_id):
 		client = MongoClient(os.environ['MONGOHQ_URL'])
 	else:
 		client = MongoClient()
-	client = MongoClient()
 	db = client.get_default_database()
 	quizzes = db.quizzes
 	return quizzes.find_one({'_id': ObjectId(mongo_id)})
